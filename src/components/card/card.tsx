47c4b72201844e539fb4e20679fb5887e49ebafa
furkan <furi544@gmail.com>
2023-02-10 18:54:23 +0300

    fixed last flip card ui issue

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -9,7 +9,7 @@ interface CardTypes extends PropsWithChildren {
 export default function Card(props: CardTypes) {
   return (
     <div
-      className={`flex flex-col justify-between basis-full md:basis-5/12 lg:basis-3/12 h-64 border shadow-md rounded bg-white p-4 ${
+      className={`flex flex-col justify-between h-64 border shadow-md rounded bg-white p-4 ${
         props.className ? props.className : ""
       }`}
     >
--- src/sections/home/what-we-do.tsx
+++ src/sections/home/what-we-do.tsx
@@ -14,16 +14,20 @@ export default function WhatWeDo() {
         </div>
       </div>
 
-      <div className="flex flex-wrap justify-center [&>:not(:last-child)]:mb-3 md:[&>:not(:last-child)]:mr-5 mt-20">
+      <div className="flex flex-wrap justify-center gap-3 mt-20 w-full">
         {cardData.map((m) => (
-          <div className="group perspective[1000px]">
+          <div className="group perspective[1000px] basis-full md:basis-5/12 lg:basis-3/12">
             <Card
               key={m.title}
               className="[transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] duration-1000"
             >
               <div>
                 <CardHeader src={m.src} alt="launchpad" />
-                <CardBody title={m.title} description={m.description} color="text-blue-gray-800"/>
+                <CardBody
+                  title={m.title}
+                  description={m.description}
+                  color="text-blue-gray-800"
+                />
               </div>
 
               <div className="absolute top-0 left-0 py-4 rounded w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)] overflow-hidden bg-blue-gray-800">
@@ -38,62 +42,40 @@ export default function WhatWeDo() {
           </div>
         ))}
 
-        <Card className="relative mr-0 md:mr-5">
-          <img
-            src="images/GALATASARAY_3D-F3 1.png"
-            alt="gs"
-            className="w-34 md:w-38 lg:w-46 h-full absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4"
-          />
-          <div className="absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 flex flex-col text-center w-full whitespace-pre-line">
-            <div className="text-blue-gray-400 text-base font-semibold md:font-bold">
-              Official 3.0, Metaverse, P2E Parthner of
-            </div>
-            <div className="text-blue-gray-800 font-bold text-2xl md:text-2xl lg:text-3xl mt-2 whitespace-pre-line">
-              GALATASARAY S.K.
-            </div>
-          </div>
-        </Card>
-
-        {/* <Card className="relative mr-0 md:mr-5">
-          <img
-            src="images/GALATASARAY_3D-F3 1.png"
-            alt="gs"
-            className="w-34 md:w-38 lg:w-46 h-full absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4"
-          />
-          <div className="absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 flex flex-col text-center w-full whitespace-pre-line">
-            <div className="text-blue-gray-400 text-base font-semibold md:font-bold">
-              Official 3.0, Metaverse, P2E Parthner of
-            </div>
-            <div className="text-blue-gray-800 font-bold text-2xl md:text-2xl lg:text-3xl mt-2 whitespace-pre-line">
-              GALATASARAY S.K.
-            </div>
-          </div>
-        </Card> */}
-
-        {/* <section className="bg-gray-900 h-screen flex justify-center items-center gap-x-16 text-white">
-          <div className="w-[300px] h-[420px] bg-transparent cursor-pointer group perspective[1000px]">
-            <div className="relative w-full h-full [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]  duration-1000">
-              <div className="absolute backface-hidden w-full h-full">
-                <img src="images/PHOTO_2.png" className="w-full h-full" />
-              </div>
-              <div className="absolute my-rotate-y-180 [backface-visibility:hidden] [transform:rotateY(180deg)] w-full h-full bg-gray-100 overflow-hidden">
-                <div className="text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24">
-                  <h1 className="text-3xl font-semibold">The King's Man</h1>
-                  <p className="my-2">9.0 Rating</p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Facilis itaque assumenda saepe animi maxime libero non
-                    quasi, odit natus veritatis enim culpa nam inventore
-                    doloribus quidem temporibus amet velit accusamus.
-                  </p>
-                  <button className="bg-teal-500 px-6 py-2 font-semibold text-white rounded-full absolute -bottom-20 delay-500 duration-1000 group-hover:bottom-20 scale-0 group-hover:scale-125">
-                    Watch Now
-                  </button>
+        <div className="w-full group perspective[1000px] basis-full md:basis-5/12 lg:basis-3/12">
+          <Card className="relative mr-0 md:mr-5 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] duration-1000 w-full">
+            <div>
+              <img
+                src="images/GALATASARAY_3D-F3 1.png"
+                alt="gs"
+                className="w-34 md:w-38 lg:w-46 h-full absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4"
+              />
+              <div className="absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 flex flex-col text-center w-full whitespace-pre-line">
+                <div className="text-blue-gray-400 text-base font-semibold md:font-bold">
+                  Official 3.0, Metaverse, P2E Parthner of
+                </div>
+                <div className="text-blue-gray-800 font-bold text-2xl md:text-2xl lg:text-3xl mt-2 whitespace-pre-line">
+                  GALATASARAY S.K.
                 </div>
               </div>
             </div>
-          </div>
-        </section> */}
+            <div className="absolute top-0 left-0 py-4 rounded w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)] overflow-hidden bg-blue-gray-800">
+              <img
+                src="images/GALATASARAY_3D-F3 1.png"
+                alt="gs"
+                className="w-34 md:w-38 lg:w-46 h-full absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4"
+              />
+              <div className="absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4 flex flex-col text-center w-full whitespace-pre-line">
+                <div className="text-white text-base font-semibold md:font-bold">
+                  Official 3.0, Metaverse, P2E Parthner of
+                </div>
+                <div className="text-white font-bold text-2xl md:text-2xl lg:text-3xl mt-2 whitespace-pre-line">
+                  GALATASARAY S.K.
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
       </div>
     </section>
   );
